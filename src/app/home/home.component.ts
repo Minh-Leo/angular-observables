053b7f99d179e18d70278a8390e773fb803efe97
Minh Bui <tranduyminh.bui@gmail.com>
2020-09-24 15:07:57 +1000

    customised observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
 
-import { interval, Subscription } from "rxjs";
+import { interval, Observable, Observer, Subscription } from "rxjs";
 
 @Component({
   selector: "app-home",
@@ -13,12 +13,40 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() {}
 
   ngOnInit() {
-    this.firstSubscription = interval(1000).subscribe((next) => {
-      console.log(next);
+    // this.firstSubscription = interval(1000).subscribe((next) => {
+    //   console.log(next);
+    // });
+
+    // How make the same observable above in the true customized way
+    // how use next error complete
+    const customIntervalObs = new Observable((observer: Observer<number>) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count == 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error("Count is more than 3"));
+        }
+        count++;
+      }, 1000);
     });
+
+    customIntervalObs.subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {
+        console.log(error.message);
+      },
+      () => {
+        console.log("Completed counting to 2");
+      }
+    );
   }
 
   ngOnDestroy(): void {
-    this.firstSubscription.unsubscribe();
+    // this.firstSubscription.unsubscribe();
   }
 }
